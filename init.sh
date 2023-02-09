759d5f3b7f379a4fa4621c888f3dc895adb9949f
Matsuzaki <ideam@88665a2476ad.ant.amazon.com>
2023-02-09 15:42:44 +0900

    Add colorscheme

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chmod +x ./scripts/link.sh
+./scripts/link.sh
--- scripts/link.sh
+++ scripts/link.sh
@@ -3,8 +3,8 @@
 dotfiles_root=$(cd $(dirname $0)/.. && pwd)
 
 # dotfilesディレクトリの中身のリンクをホームディレクトリ直下に作成
-cd ${dotfiles_root}/dotfiles
-for file in .*; do
-    ln -s ${PWD}/${file} ${HOME}
+cd ${dotfiles_root}/dotfiles/.config/
+for file in *; do
+    echo "make link to ${PWD}/${file}"
+    ln -s ${PWD}/${file} ${HOME}/.config
 done
-

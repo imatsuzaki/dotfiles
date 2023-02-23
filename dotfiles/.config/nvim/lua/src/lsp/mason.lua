25ed828fee0cb926db668477ba5a2ec27d1b876b
imatsuzaki <idea.matsuzaki@gmail.com>
2023-02-24 00:05:00 +0900

    Add lsp setting but not still not complete

--- dotfiles/.config/nvim/lua/src/lsp/mason.lua
+++ dotfiles/.config/nvim/lua/src/lsp/mason.lua
@@ -43,8 +43,8 @@ for _, server in pairs(servers) do
 
 	server = vim.split(server, "@")[1]
 
-	local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
-	if require_ok then
+  local require_ok, conf_opts = pcall(require, "user.src.settings." .. server)
+  if require_ok then
 		opts = vim.tbl_deep_extend("force", conf_opts, opts)
 	end
 
--- dotfiles/.config/nvim/lua/src/plugins.lua
+++ dotfiles/.config/nvim/lua/src/plugins.lua
@@ -56,6 +56,12 @@ return packer.startup(function(use)
   use { "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" } -- snippet completions
 	use { "hrsh7th/cmp-nvim-lsp", commit = "3cf38d9c957e95c397b66f91967758b31be4abe6" }
 	use { "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" }
+  use "hrsh7th/vim-vsnip"
+
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
